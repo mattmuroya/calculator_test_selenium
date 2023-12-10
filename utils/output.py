1b0dba7d2374d7ee3025c377556dc83af255233e
Matt Muroya <13994435+mattmuroya@users.noreply.github.com>
2023-12-09 17:58:55 -1000

    Refactor/comment test_runner and output utils

--- utils/output.py
+++ utils/output.py
@@ -1,10 +1,25 @@
-"""Functions for printing test results.
+"""Functions for printing test results to the console.
 """
 
 def print_test_begin():
+    """Prints BEGINNING TEST EXECUTION banner.
+    """
     print("\n============== BEGINNING TEST EXECUTION ===============\n")
 
+def print_pass(test, param_set):
+    """Prints test name and parameter set for a passed test case.
+    """
+    print("✅ PASS:", test.__name__, param_set)
+
+def print_fail(test, param_set):
+    """Prints test name and parameter set for a failed test case.
+    """
+    print("❌ FAIL:", test.__name__, param_set)
+
 def print_test_end(passed, total, time):
+    """Prints TEST EXECUTION COMPLETE banner with percentage of passed test
+    cases and total execution time.
+    """
     if total > 0:
         percent = format(passed / total, ".0%")
         print(f"\n===== TEST EXECUTION COMPLETE: {passed} / {total} ({percent}) in {time} =====\n")
--- utils/test_runner.py
+++ utils/test_runner.py
@@ -1,42 +1,49 @@
-"""Defines functions used for executing test case suites.
+"""Functions for executing test suites and test cases.
 """
 
-from utils.output import print_test_begin, print_test_end
-import time, datetime
+from time import time
+from datetime import timedelta
+from utils import output
 
 def execute_test_case(test, param_sets):
-    """Takes a test definition, a list of parameter sets, and passed/total
-    counters and executes the test for each set of data in the params list,
-    incrementing the passed/total counters each time.
+    """Takes a test definition with a list of parameter sets and executes the
+    test once for each set. Returns the number of passed/total executions.
     """
+    # Initialize counters
     passed = 0
     total = 0
 
+    # Execute test for each parameter set, increment counters
     for param_set in param_sets:
         try:
             test(*param_set)
             passed += 1
-            print("✅ PASS:", test.__name__)
+            output.print_pass(test, param_set)
         except AssertionError:
-            print("❌ FAIL:", test.__name__, param_set)
+            output.print_fail(test, param_set)
         total +=1
 
+    # Return number of passed/total executions
     return (passed, total)
 
 def execute_tests(test_cases):
-    """Takes a list of tuples, each of which contains a test case and list of
-    parameter sets, and executes each pair in sequence.
+    """Takes a list of test cases (test definition with a list of parameter
+    sets) and executes each test definition/parameter set pairing in sequence.
     """
+    # Initialize counters
     passed = 0
     total = 0
 
-    print_test_begin()
-    t0 = time.time()
+    # Begin test suite execution and log start time
+    output.print_test_begin()
+    t0 = time()
 
+    # Execute each test definition and increment passed/total execution counts.
     for test_case in test_cases:
-        d_passed, d_total = execute_test_case(test_case[0], test_case[1])
-        passed += d_passed
-        total += d_total
+        p_increment, t_increment = execute_test_case(test_case[0], test_case[1])
+        passed += p_increment
+        total += t_increment
 
-    t1 = time.time()
-    print_test_end(passed, total, datetime.timedelta(seconds=int(t1-t0)))
+    # Log end time and report final results
+    t1 = time()
+    output.print_test_end(passed, total, timedelta(seconds=int(t1-t0)))
